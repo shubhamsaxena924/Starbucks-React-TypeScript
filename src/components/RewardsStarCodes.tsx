b06557fde29b66ad661068205302bcd4a71a4a85
shubhamsaxena924 <shubham.saxena_cs.ccv19@gla.ac.in>
2021-08-03 01:12:59 +0530

    manual form validation added

--- src/components/RewardsStarCodes.tsx
+++ src/components/RewardsStarCodes.tsx
@@ -5,6 +5,34 @@ interface Props {}
 
 const RewardsStarCodes: React.FC<Props> = (props) => {
   const [isInputFocussed, setIsInputFocussed] = useState<boolean>(false);
+  const [starCode, setStarCode] = useState<string>("");
+  const [isTouched, setIsTouched] = useState<boolean>(false);
+  let fieldError: string = "";
+  if (!starCode) {
+    fieldError = "Please enter a Star code.";
+  }
+  let labelClass: string = "";
+  const classesOnTop: string =
+    " -translate-y-3/4 -translate-x-5 transform text-primary-300 scale-75 duration-300 ";
+  const classesInCenter: string =
+    " translate-y-0 translate-x-0 transform text-gray-700 duration-300 ";
+  if (isInputFocussed && fieldError && isTouched) {
+    labelClass = "text-youtube" + classesOnTop;
+  } else if (isInputFocussed && fieldError && !isTouched) {
+    labelClass = "text-primary-300" + classesOnTop;
+  } else if (isInputFocussed && !fieldError && isTouched) {
+    labelClass = "text-primary-300" + classesOnTop;
+  } else if (isInputFocussed && !fieldError && !isTouched) {
+    labelClass = "text-primary-300" + classesOnTop;
+  } else if (!isInputFocussed && fieldError && isTouched) {
+    labelClass = "text-youtube" + classesInCenter;
+  } else if (!isInputFocussed && fieldError && !isTouched) {
+    labelClass = "text-gray-700" + classesInCenter;
+  } else if (!isInputFocussed && !fieldError && isTouched) {
+    labelClass = "text-gray-700" + classesOnTop;
+  } else if (!isInputFocussed && !fieldError && !isTouched) {
+    labelClass = "text-gray-700" + classesOnTop;
+  }
   return (
     <>
       <div className="mb-20 md:mb-32">
@@ -17,30 +45,80 @@ const RewardsStarCodes: React.FC<Props> = (props) => {
             </p>
             <form>
               <div className="p-2">
-                <div className="mb-4">
-                  <label>
+                <div className="relative mb-2">
+                  {fieldError && isTouched && (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="absolute w-6 h-6 text-youtube top-3 right-3"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  )}
+                  <label htmlFor="star-code">
                     <span
                       className={
-                        (isInputFocussed
-                          ? "-translate-y-3/4 -translate-x-5 text-primary-300 scale-75 transform duration-300"
-                          : "translate-y-0 translate-x-0 text-gray-700 transform duration-300") +
+                        labelClass +
                         " absolute m-2 p-1 px-2 text-lg tracking-wider font-semibold  bg-white"
                       }
                     >
                       Enter your Star Code
                     </span>
+
                     <input
+                      id="star-code"
                       type="text"
                       name="star-code"
-                      className="w-full p-3 text-lg font-medium rounded-lg outline-none ring-1 ring-gray-400 focus:ring-2 focus:ring-primary-300"
+                      required
+                      className={
+                        (fieldError && isTouched
+                          ? "focus:ring-youtube ring-youtube"
+                          : "focus:ring-primary-300 ring-primary-300") +
+                        " w-full p-3 text-lg font-medium rounded-lg outline-none ring-1 ring-gray-400 focus:ring-2 "
+                      }
                       onFocus={() => setIsInputFocussed(() => true)}
-                      onBlur={() => setIsInputFocussed(() => false)}
+                      onBlur={() => {
+                        setIsInputFocussed(() => false);
+                        setIsTouched(() => true);
+                      }}
+                      value={starCode}
+                      onChange={(event) => setStarCode(event.target.value)}
                     />
                   </label>
+                  {isTouched && fieldError && (
+                    <div>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="inline w-5 h-5 m-1 text-youtube"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                      <span className="text-sm">{fieldError}</span>
+                    </div>
+                  )}
                 </div>
                 <div></div>
               </div>
-              <button className="block px-4 py-2 ml-auto font-semibold duration-300 border border-black rounded-full hover:bg-primary-100">
+              <button
+                onClick={(event) => {
+                  console.log("Star Code is: " + starCode);
+                  event.preventDefault();
+                }}
+                className="block px-4 py-2 ml-auto font-semibold duration-300 border border-black rounded-full hover:bg-primary-100"
+              >
                 Submit
               </button>
             </form>
